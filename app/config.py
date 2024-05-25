f5fc84e4687a6527dadca20b69d4cc645319bf67
montelongoeric7 <montelongo_eric@berkeley.edu>
2024-05-24 17:44:32 -0700

    Update configuration settings

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class Settings(BaseSettings):
     database_hostname: str
-    database_port: str
+    database_port: int
     database_password: str
     database_name: str
     database_username: str
